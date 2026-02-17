d19cf2ec7c92c9ceb8ca190ef32f971145956e1e
Baireddy Venkata Devendharreddy <123855834+Dev0602@users.noreply.github.com>
2026-02-16 21:39:35 -0500

    Trigger registration by importing client module
    Import slack_client_impl.client to trigger registration.

--- components/slack_client_impl/src/slack_client_impl/__init__.py
+++ components/slack_client_impl/src/slack_client_impl/__init__.py
@@ -1,4 +1,7 @@
 """Slack chat client implementation."""
 from slack_client_impl.client import SlackClient as SlackClient
 
+# Importing this module triggers registration in client.py
+import slack_client_impl.client  # noqa: F401
+
 __version__ = "0.1.0"
