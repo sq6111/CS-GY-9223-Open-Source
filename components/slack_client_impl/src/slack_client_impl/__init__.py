052ea4fa022053ae58360a933724692ab425ef7f
Baireddy Venkata Devendharreddy <123855834+Dev0602@users.noreply.github.com>
2026-02-17 02:48:00 +0000

    Fix integration test and import ordering - all tests passing

--- components/slack_client_impl/src/slack_client_impl/__init__.py
+++ components/slack_client_impl/src/slack_client_impl/__init__.py
@@ -1,7 +1,6 @@
 """Slack chat client implementation."""
-from slack_client_impl.client import SlackClient as SlackClient
-
 # Importing this module triggers registration in client.py
 import slack_client_impl.client  # noqa: F401
+from slack_client_impl.client import SlackClient as SlackClient
 
 __version__ = "0.1.0"
